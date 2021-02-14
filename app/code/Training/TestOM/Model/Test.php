e8611bb18d2f389e4ca93e3c51ba0f01595b8573
Oleh K <oleg.kh.arpo@gmail.com>
2021-02-14 15:02:18 +0200

    HW2: 1.2. di.xml part 1

---
+++ app/code/Training/TestOM/Model/Test.php
@@ -0,0 +1,32 @@
+<?php
+namespace Training\TestOM\Model;
+
+class Test
+{
+    private $manager;
+    private $arrayList;
+    private $name;
+    private $number;
+    public function __construct(
+        \Training\TestOM\Model\Manager $manager,
+        $name,
+        int $number,
+        array $arrayList
+    ) {
+        $this->manager = $manager;
+        $this->name = $name;
+        $this->number = $number;
+        $this->arrayList = $arrayList;
+    }
+
+    public function log()
+    {
+        print_r(get_class($this->manager));
+        echo '<br>';
+        print_r($this->name);
+        echo '<br>';
+        print_r($this->number);
+        echo '<br>';
+        print_r($this->arrayList);
+    }
+}
