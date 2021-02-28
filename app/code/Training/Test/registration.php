a6d685664d0fb822faad1bf18a6070525cff6a3b
Oleh K <oleg.kh.arpo@gmail.com>
2021-03-01 00:55:40 +0200

    HW:2.4

--- app/code/Training/Test/registration.php
+++ app/code/Training/Test/registration.php
@@ -1,3 +1,6 @@
 <?php
-use \Magento\Framework\Component\ComponentRegistrar;
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Training_Test', __DIR__);
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Training_Test',
+    __DIR__
+);
