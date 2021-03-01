7655b8036cecfedb9140c30fe9835f15f80f2070
Oleh K <oleg.kh.arpo@gmail.com>
2021-03-01 02:14:58 +0200

    HW:3.3

--- app/code/Training/Test/Controller/Block/Index.php
+++ app/code/Training/Test/Controller/Block/Index.php
@@ -3,25 +3,27 @@ namespace Training\Test\Controller\Block;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
-    /**
-    * @var \Magento\Framework\View\LayoutFactory
-    */
     private $layoutFactory;
-    /**
-    * @param \Magento\Backend\App\Action\Context $context
-    * @param \Magento\Framework\View\LayoutFactory $layoutFactory
-    */
+
+    private $rawResultFactory;
+
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\View\LayoutFactory $layoutFactory
+        \Magento\Framework\View\LayoutFactory $layoutFactory,
+        \Magento\Framework\Controller\Result\RawFactory $rawResultFactory
     ) {
         $this->layoutFactory = $layoutFactory;
+        $this->rawResultFactory = $rawResultFactory;
         parent::__construct($context);
     }
     public function execute()
     {
         $layout = $this->layoutFactory->create();
         $block = $layout->createBlock('Training\Test\Block\Test');
-        $this->getResponse()->appendBody($block->toHtml());
+//        $this->getResponse()->appendBody($block->toHtml());
+        $result = $this->rawResultFactory->create();
+        $result->setHeader('Content-Type', 'text/xml');
+        $result->setContents('<root><science>'.$block->toHtml().'</science></root>');
+        return $result;
     }
 }
