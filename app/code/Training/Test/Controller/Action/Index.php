62d56536b618a58dd75fd6c9cad7047a988b1661
Oleh K <oleg.kh.arpo@gmail.com>
2021-03-01 02:44:37 +0200

    HW:3.6

---
+++ app/code/Training/Test/Controller/Action/Index.php
@@ -0,0 +1,28 @@
+<?php
+namespace Training\Test\Controller\Action;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    private $resultRawFactory;
+
+    private $layoutFactory;
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory,
+        \Magento\Framework\View\LayoutFactory $layoutFactory
+    ) {
+        $this->resultRawFactory = $resultRawFactory;
+        $this->layoutFactory = $layoutFactory;
+        parent::__construct($context);
+    }
+    public function execute()
+    {
+        $layout = $this->layoutFactory->create();
+        $block = $layout->createBlock('Training\Test\Block\Test');
+        $block->setTemplate('test.phtml');
+        /** @var \Magento\Framework\Controller\Result\Raw $resultRaw */
+        $resultRaw = $this->resultRawFactory->create();
+        return $resultRaw->setContents($block->toHtml());
+    }
+}
