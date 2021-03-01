a34074af311f6b632365911f9e15836b28e0ae2f
Oleh K <oleg.kh.arpo@gmail.com>
2021-03-01 02:51:26 +0200

    HW:3.7

--- app/code/Training/Test/Plugin/Block/Product/View/Description.php
+++ app/code/Training/Test/Plugin/Block/Product/View/Description.php
@@ -3,9 +3,14 @@ namespace Training\Test\Plugin\Block\Product\View;
 
 class Description extends \Magento\Catalog\Block\Product\View\Description
 {
+//    public function beforeToHtml(
+//        \Magento\Catalog\Block\Product\View\Description $subject
+//    ) {
+//        $subject->getProduct()->setDescription('Test description');
+//    }
     public function beforeToHtml(
         \Magento\Catalog\Block\Product\View\Description $subject
     ) {
-        $subject->getProduct()->setDescription('Test description');
+        $subject->setTemplate('Training_Test::description.phtml');
     }
 }
