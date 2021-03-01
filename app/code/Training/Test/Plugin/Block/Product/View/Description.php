b99545be8801deb09b9ba706c63904f06189c72b
Oleh K <oleg.kh.arpo@gmail.com>
2021-03-01 02:56:10 +0200

    HW:3.8

--- app/code/Training/Test/Plugin/Block/Product/View/Description.php
+++ app/code/Training/Test/Plugin/Block/Product/View/Description.php
@@ -8,9 +8,16 @@ class Description extends \Magento\Catalog\Block\Product\View\Description
 //    ) {
 //        $subject->getProduct()->setDescription('Test description');
 //    }
+//    public function beforeToHtml(
+//        \Magento\Catalog\Block\Product\View\Description $subject
+//    ) {
+//        $subject->setTemplate('Training_Test::description.phtml');
+//    }
     public function beforeToHtml(
         \Magento\Catalog\Block\Product\View\Description $subject
     ) {
-        $subject->setTemplate('Training_Test::description.phtml');
+        if ($subject->getNameInLayout() === 'product.info.sku') {
+            $subject->setTemplate('Training_Test::description.phtml');
+        }
     }
 }
