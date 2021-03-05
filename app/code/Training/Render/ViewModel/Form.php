d06a4abb5987655281ba3bfa53f15c01a53efef7
Oleh K <oleg.kh.arpo@gmail.com>
2021-03-06 01:41:53 +0200

    HW:3.14

---
+++ app/code/Training/Render/ViewModel/Form.php
@@ -0,0 +1,23 @@
+<?php
+namespace Training\Render\ViewModel;
+
+class Form implements \Magento\Framework\View\Element\Block\ArgumentInterface
+{
+    /**
+     * @var \Magento\Framework\UrlInterface
+     */
+    public $urlBuilder;
+    /**
+     * @param \Magento\Framework\UrlInterface $urlBuilder
+     */
+    public function __construct(
+        \Magento\Framework\UrlInterface $urlBuilder
+    ) {
+        $this->urlBuilder = $urlBuilder;
+    }
+    public function getSubmitUrl()
+    {
+        return $this->urlBuilder->getUrl('customer/account/login');
+    }
+
+}
